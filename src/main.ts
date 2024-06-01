e0e593b9f94923e18372ca55172433446421635b
Dmytro Horbatenko <dmytro.horbatenko@redduck.io>
2024-06-01 14:11:35 +0300

    fix: add secure cors

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,10 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   app.use(cookieParser());
   app.useGlobalInterceptors();
   app.useGlobalPipes(
